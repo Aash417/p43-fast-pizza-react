b71e650ca19582a1e4f22903a428d13b1930eeb1
aashish_k <aashishkathat00@gmail.com>
2023-12-19 00:37:03 +0530

    fetching data with react router and usine useNavigation

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+	return (
+		<div>
+			<Link to="/">Fast Pizza co.</Link>
+			<p>aashish & co.</p>
+		</div>
+	);
+}
+
+export default Header;
