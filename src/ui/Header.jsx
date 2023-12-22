5751180f25f862f5b42bd8d05629d5052bf69eb9
aashish_k <aashishkathat00@gmail.com>
2023-12-23 00:11:54 +0530

    responsive design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,8 @@ import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
 	return (
-    <header className="border-b border-stone-200 bg-yellow-500 uppercase">
-      <Link to="/" className="tracking-[5px]">
+    <header className="border-b border-stone-200 bg-yellow-500 uppercase sm:px-6">
+      <Link to="/" className="tracking-[5px] ">
         Fast Pizza co.
       </Link>
       <SearchOrder />
