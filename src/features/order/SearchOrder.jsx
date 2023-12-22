fba5c854341a936be1140e5b1c34e1f0b96af033
aashish_k <aashishkathat00@gmail.com>
2023-12-23 01:08:17 +0530

    styling form el

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -14,14 +14,15 @@ function SearchOrder() {
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				placeholder="Search order "
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-		</form>
-	);
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order "
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300  placeholder:text-stone-500 focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64"
+      />
+    </form>
+  );
 }
 
 export default SearchOrder;
