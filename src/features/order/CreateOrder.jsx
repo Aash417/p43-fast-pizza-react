84c4a8f1c586a4f2bf08ec0d8cbb925c11fa8e06
aashish_k <aashishkathat00@gmail.com>
2023-12-23 22:41:30 +0530

    reusing style with react component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,60 +1,61 @@
 import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
-	/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-		str
-	);
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
 
 const fakeCart = [
-	{
-		pizzaId: 12,
-		name: 'Mediterranean',
-		quantity: 2,
-		unitPrice: 16,
-		totalPrice: 32,
-	},
-	{
-		pizzaId: 6,
-		name: 'Vegetale',
-		quantity: 1,
-		unitPrice: 13,
-		totalPrice: 13,
-	},
-	{
-		pizzaId: 11,
-		name: 'Spinach and Mushroom',
-		quantity: 1,
-		unitPrice: 15,
-		totalPrice: 15,
-	},
+  {
+    pizzaId: 12,
+    name: 'Mediterranean',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: 'Vegetale',
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: 'Spinach and Mushroom',
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
 ];
 
 function CreateOrder() {
-	const navigation = useNavigation();
-	const isSubmitting = navigation.state === 'submitting';
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
 
-	const formErrors = useActionData();
+  const formErrors = useActionData();
 
-	// const [withPriority, setWithPriority] = useState(false);
-	const cart = fakeCart;
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
 
-	return (
+  return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
       <Form method="post">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,12 +63,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              className="w-full rounded-full border border-s px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
-              type="text"
-              name="address"
-              required
-            />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
@@ -85,12 +81,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Placing order ...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
@@ -98,23 +91,23 @@ function CreateOrder() {
 }
 
 export async function action({ request }) {
-	const formData = await request.formData();
-	const data = Object.fromEntries(formData);
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
 
-	const order = {
-		...data,
-		cart: JSON.parse(data.cart),
-		priority: data.priority === 'on',
-	};
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on',
+  };
 
-	const errors = {};
-	if (!isValidPhone(order.phone))
-		errors.phone = 'please give us your correct phone no.';
-	if (Object.keys(errors).length > 0) return errors;
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone = 'please give us your correct phone no.';
+  if (Object.keys(errors).length > 0) return errors;
 
-	// if everything is ok create new order and redirect
-	const newOrder = await createOrder(order);
-	return redirect(`/order/${newOrder.id}`);
+  // if everything is ok create new order and redirect
+  const newOrder = await createOrder(order);
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
