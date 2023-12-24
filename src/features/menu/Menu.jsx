6d0fba86bb86893e837c39344231b9989233b7d7
aashish_k <aashishkathat00@gmail.com>
2023-12-24 16:10:28 +0530

    styling the components

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -6,12 +6,12 @@ function Menu() {
 	const menu = useLoaderData();
 
 	return (
-		<ul>
-			{menu.map((pizza) => (
-				<MenuItem pizza={pizza} key={pizza.id} />
-			))}
-		</ul>
-	);
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
