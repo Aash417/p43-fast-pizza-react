08a6c3df283d7888017e15d994d502af8bdd3bb5
aashish_k <aashishkathat00@gmail.com>
2023-12-25 23:48:35 +0530

    createAsyncThunk in action

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
@@ -7,6 +7,7 @@ import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
 import { formatCurrency } from '../../utils/helpers';
 import { useState } from 'react';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -43,6 +44,7 @@ function CreateOrder() {
   const username = useSelector((state) => state.user.username);
   const cart = useSelector(getCart);
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
@@ -56,6 +58,9 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>click</button>
+
       <Form method="post">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
