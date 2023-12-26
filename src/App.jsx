284cfa7126bf3a5e2e650279c1e23f6215128743
aashish_k <aashishkathat00@gmail.com>
2023-12-27 00:22:09 +0530

    updating form with out navigation (usefetcher)

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,41 @@ import CreateOrder, {
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 
 const router = createBrowserRouter([
-	{
-		element: <AppLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-			},
-			{
-				path: '/menu',
-				element: <Menu />,
-				errorElement: <Error />,
-				loader: menuLoader,
-			},
-			{
-				path: '/cart',
-				element: <Cart />,
-			},
-			{
-				path: '/order/new',
-				element: <CreateOrder />,
-				action: createOrderAction,
-			},
-			{
-				path: '/order/:orderId',
-				element: <Order />,
-				loader: orderLoader,
-				errorElement: <Error />,
-			},
-		],
-	},
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        errorElement: <Error />,
+        loader: menuLoader,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+    ],
+  },
 ]);
 
 function App() {
---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,24 @@
+import { useFetcher } from 'react-router-dom';
+import Button from '../../ui/Button';
+import { updateOrder } from '../../services/apiRestaurant';
+
+function UpdateOrder({ order }) {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="Patch" className="text-right">
+      <Button type="primary">Make priority</Button>;
+    </fetcher.Form>
+  );
+}
+
+export default UpdateOrder;
+
+export async function action({ request, params }) {
+  const data = { priority: true };
+  const orderId = params.orderId;
+
+  await updateOrder(orderId, data);
+
+  return null;
+}
